47d661b480e1d3ccaf17f2bf29f625dc5f51cafa
Ricardo Aguilar <rickbaset7@gmail.com>
2024-01-10 22:24:37 -0600

    Getting started with mongoose and mongoDB database

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
+
+/**
+ * Use mongoDB to connect to the database.
+ */
+
+app.use('/', (req, res) => {
+    res.send("Hello from the server side");
+});
+
+/**
+ * Listen on provided port.
+ */
+
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
 });
